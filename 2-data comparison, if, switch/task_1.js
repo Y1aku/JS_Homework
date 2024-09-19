42a0a552091c33f5332eef792c15d3e8b221abbc
Kirill <stoporenko.k@gmail.com>
2024-09-20 01:49:44 +0700

    solved task_1 + added task_2 and task_3 files

---
+++ 2-data comparison, if, switch/task_1.js
@@ -0,0 +1,17 @@
+// Создайте переменную salary со значением 1000
+
+const salary = 1000;
+
+/*
+Создайте переменную grade, которая должна
+получить значение "middle" если salary больше или равна 1000,
+и значение "junior" - если меньше
+*/
+
+let grade;
+
+salary >= 1000 ? grade = "middle" : grade = "junior"
+
+// Проверка
+
+console.log(grade);
