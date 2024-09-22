38e9475dcb840dc1250774886e86620292056b04
Kirill <stoporenko.k@gmail.com>
2024-09-23 02:50:38 +0700

    solved task 3 form 2-data_comparison_if_switch

--- 2-data comparison, if, switch/task_3.js
+++ 2-data comparison, if, switch/task_3.js
@@ -9,31 +9,17 @@ const minAge = 18;
 const maxAge = 60;
 let age;
 
-age = 19
+age = "60"
 
-switch(true) {
-
-    case typeof age !== 'number': {
-        console.log("Incorrect data type");
-        break;
-    }
-
-    case age < minAge: {
-        console.log("You don't have access cause your age is " + age + ". It's less than " + minAge);
-        break;
-    }
-
-    case age >= minAge && age < maxAge: {
-        console.log("Welcome!");
-        break;
-    }
-
-    case age > maxAge: {
-        console.log("Keep calm and look Culture channel");
-        break;
-    }
-
-    default :{
-		console.log("Technical work");
-	}
+if (typeof age != "number" && isNaN(age) == false) {
+    age = +age;
+    console.log(`Your age is ${age}`);
+} else if (age < minAge) {
+    console.log("You don't have access cause your age is " + age + ". It's less than " + minAge);
+} else if (age >= minAge && age < maxAge) {
+    console.log("Welcome!");
+} else if (age > maxAge) {
+    console.log("Keep calm and look Culture channel");
+} else {
+    console.log("Technical work");
 }
