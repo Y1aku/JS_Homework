3dd5f2a871b8f6e4d8853ab90617644bdc97262a
Kirill <stoporenko.k@gmail.com>
2024-09-23 02:33:10 +0700

    fixed task 2 from 2-data_comparison_if_switch

--- 2-data comparison, if, switch/task_2.js
+++ 2-data comparison, if, switch/task_2.js
@@ -12,8 +12,23 @@ let age;
 
 // Создать if в котором будете проверять весь код переменной age со следующими условиями:
 
-age = 19
+age = 60
 
+if (typeof age != "number") {
+    console.log("Incorrect data type");
+} else if (age < minAge) {
+    console.log("You don't have access cause your age is " + age + ". It's less than " + minAge);
+} else if (age >= minAge && age < maxAge) {
+    console.log("Welcome!");
+} else if (age > maxAge) {
+    console.log("Keep calm and look Culture channel");
+} else {
+    console.log("Technical work");
+}
+
+
+
+/*
 switch(true) {
 
     // Добавьте проверку: если тип данных в переменной age не number - вывести в консоль Incorrect data type
@@ -50,3 +65,4 @@ switch(true) {
 		console.log("Technical work");
 	}
 }
+*/
