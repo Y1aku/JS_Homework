072ab461b3b6575ca51d58fd6905dad3e2824f32
Kirill <stoporenko.k@gmail.com>
2024-09-23 23:07:08 +0700

    fixed task 3 from 3-loops_string_number

--- 3-loops_string_number/task_3.js
+++ 3-loops_string_number/task_3.js
@@ -5,33 +5,28 @@
 Ответ должен выводиться шаблонным литералом вида word contains x vowels and y consonants
 */
 
-const word = "Пенал";
+const word = "Летний день";
 const countOfLetters = word.length;
 const vowelsKit = "аеёиоуыэюяАЕЁИОУЫЭЮЯ";
 const countOfVowelsInKit = vowelsKit.length;
+const consonantsKit = "бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ";
+const countOfConsonantsInKit = consonantsKit.length;
 
 let countOfVowels = 0;
 let countOfConsonants = 0;
 
 for (let i = 0; i < countOfLetters; i++) {
 
-    let isVowel = false;
-
-    for (let j = 0; j < countOfVowelsInKit; j++) {
+    for (let j = 0; j < countOfConsonantsInKit; j++) {
         if (word[i].includes(vowelsKit[j])) {
             countOfVowels = ++countOfVowels;
-            // console.log("Кол-во гласных - " + countOfVowels);
-            // console.log(`${word[i]} - это гласная`);
-            isVowel = true;
+            break;
+        } else if (word[i].includes(consonantsKit[j])) {
+            countOfConsonants = ++countOfConsonants;
             break;
         }
     }
 
-    if (!isVowel) {
-        countOfConsonants = ++countOfConsonants;
-        // console.log("Кол-во согласных - " + countOfConsonants);
-        // console.log(`${word[i]} - это согласная`);
-    }
 }
 
-console.log(`word contains ${countOfVowels} vowels and ${countOfConsonants} consonants`)
+console.log(`word contains ${countOfVowels} vowels and ${countOfConsonants} consonants`);
