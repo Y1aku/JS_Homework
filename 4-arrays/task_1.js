b701705bbdbb8a41d328d9e320cbe18f36582a30
Kirill <stoporenko.k@gmail.com>
2024-09-27 05:56:33 +0700

    solved task 1.2 and changed name of array from task 1.1

--- 4-arrays/task_1.js
+++ 4-arrays/task_1.js
@@ -5,9 +5,9 @@
 - если элемент нечетный - возведет его в 3ю степень
 */
 
-const array = [1,2,3,4,5,6,7,8,9,10];
+const arrayOfTask1 = [1,2,3,4,5,6,7,8,9,10];
 
-for (let element of array) {
+for (let element of arrayOfTask1) {
 
     if (element % 2) {
         element = element ** 3;
@@ -17,3 +17,20 @@ for (let element of array) {
         console.log(element);
     }
 }
+
+/*
+Методы массивов
+- Создайте массив [1,2,3,4,5]
+- Добавьте в конец массива число 6 соответствующим методом
+- Добавьте в начало массива число 0 соответствующим методом
+- Удалите элемент с индексом 2 из массива соответствующим методом
+- Удалите последний элемент из массива соответствующим методом
+- В результате вы должны получить [0, 1, 3, 4, 5]
+*/
+
+const arrayOfTask2 = [1,2,3,4,5];
+arrayOfTask2.push(6);
+arrayOfTask2.unshift(0);
+arrayOfTask2.splice(2, 1);
+arrayOfTask2.pop();
+console.log(arrayOfTask2);
