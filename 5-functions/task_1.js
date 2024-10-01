9ce85defa5bf7c7fbea6ec0c9d987d82241a0e52
Kirill <stoporenko.k@gmail.com>
2024-10-01 20:41:13 +0700

    solved task 1.2

--- 5-functions/task_1.js
+++ 5-functions/task_1.js
@@ -5,17 +5,41 @@
 -  Решить с использованием Spread operator
 */
 
-const mergeArrays = [[1, 2], [3, 4], [5, 6]];
+// const mergeArrays = [[1, 2], [3, 4], [5, 6]];
 
-function spreadArrays(...arrays) {
+// function spreadArrays(...arrays) {
 
-    const newArray = [];
+//     const newArray = [];
 
-    for (const array of arrays) {
-        newArray.push(...array);
+//     for (const array of arrays) {
+//         newArray.push(...array);
+//     }
+
+//     return newArray;
+// }
+
+// console.log(spreadArrays(...mergeArrays));
+
+
+/*
+Написать функцию, которая преобразует любое предложение в вот_Такой_Вот_Вид и возвращает его. 
+-  Первое слово должно начинаться с буквы в нижнем регистре, у остальных -  верхнем. 
+-  Пример: I am super engineer => i_Am_Super_Engineer
+*/
+
+const sentence = "I am super engineer";
+
+function changeSentence(input) {
+    const newSentence = input.split(" ");
+    for (let i = 0; i < newSentence.length; i++) {
+        if (i === 0) {
+            newSentence[i] = newSentence[i][i].toLowerCase() + newSentence[i].slice(1);
+        } else {
+            newSentence[i] = newSentence[i][0].toUpperCase() + newSentence[i].slice(1);
+        }
     }
-
-    return newArray;
+    return newSentence.join("_");
 }
 
-console.log(spreadArrays(...mergeArrays));
+console.log(changeSentence(sentence));
+
