38aab6a0035277b48deb8b4bcaf6fa98ea219b8e
Kirill <stoporenko.k@gmail.com>
2024-10-02 00:14:07 +0700

    solved task 2.2

--- 5-functions/task_2.js
+++ 5-functions/task_2.js
@@ -2,8 +2,8 @@
 Написать функцию, которая принимает на вход слово и проверяет, является ли это слово палиндромом
 */
 
-// const word = "Lol";
-const word = "Text";
+const word = "Lol";
+// const word = "Text";
 
 function checkPalindrome(input) {
     const arrayOfInput = input.toLowerCase().split("");
@@ -23,5 +23,27 @@ function checkPalindrome(input) {
 console.log(checkPalindrome(word));
 
 /*
-
+Написать функцию, которая принимает предложение (слова разделенные только пробелами) в качестве параметра
+и возвращает слово с наибольшим количеством букв. Если таких слов несколько - возвращает их все.
 */
+
+const sentence = "I am super engineer";
+// const sentence = "I am super puper boy";
+
+function findMaxLength(input) {
+    const newArray = input.split(" ");
+    let wordMaxLength = 0;
+    let arrayOfMaxLengthWords = [];
+
+    for (const element of newArray) {
+        if (element.length > wordMaxLength) {
+            wordMaxLength = element.length;
+            arrayOfMaxLengthWords = [element];
+        } else if (element.length === wordMaxLength) {
+            arrayOfMaxLengthWords.push(element);
+        }
+    }
+    return arrayOfMaxLengthWords;
+}
+
+console.log(findMaxLength(sentence));
