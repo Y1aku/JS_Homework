5acf9ab9c38b9ec5c0422a8ac8121a23414ccc52
Kirill <stoporenko.k@gmail.com>
2024-10-03 02:53:05 +0700

    resolved task 3 based on new requirements

--- 4-arrays/task_3.js
+++ 4-arrays/task_3.js
@@ -5,25 +5,14 @@
 */
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
+const arrayWithoutDublicates = [];
 
-for (let i = arr.length - 1; i >= 0; i--) {
-
-    let countOfIndex = 0;
-
-    for (let j = 0; j < arr.length; j++) {
-
-        if (arr[i] === arr[j]) {
-            countOfIndex++;
-            // console.log(arr[i], arr[j], countOfIndex);
-        } /* else {
-            console.log(arr[i], arr[j], countOfIndex);
-        } */
-
-        if (countOfIndex >= 2) {
-            arr.splice(i, 1);
-            // console.log(arr);
-        }
+for (let i = 0; i < arr.length; i++) {
+    if (arrayWithoutDublicates.includes(arr[i])) {
+        continue;
+    } else {
+        arrayWithoutDublicates.push(arr[i]);
     }
 }
 
-console.log(arr);
+console.log(arrayWithoutDublicates);
