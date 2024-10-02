88aa999474ebafbc3589a88cf22576c072179397
Kirill <stoporenko.k@gmail.com>
2024-10-03 02:29:59 +0700

    refactored task 4

--- 3-loops_string_number/task_4.js
+++ 3-loops_string_number/task_4.js
@@ -4,57 +4,35 @@
 Например let str = 'AbC'; let offset = -1, result = 'ZaB'; Например let str = 'ZzZ'; let offset = 1, result = 'AaA';
 */
 
-const acceptedWord = "Патрик";
+const acceptedWord = "АЯая-п";
 const offset = -1;
-const kitsLength = 33;
-const lowercaseLettersKit = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-const uppercaseLettersKit = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-
 let outputWord = "";
+
+const lettersKit = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
 
 for (let i = 0; i < acceptedWord.length; i++) {
 
-	let searchLetter = acceptedWord[i];
-	let indexOfWordInKit = 0;
-	let newIndex = 0;
+    let indexOfLettersKit = 0;
 
-	if (offset === 1) {
+    if (lettersKit.includes(acceptedWord.charAt(i))) {
+        indexOfLettersKit = lettersKit.indexOf(acceptedWord.charAt(i));
 
-		if (uppercaseLettersKit.includes(searchLetter)) {
-			// console.log(searchLetter);
-			indexOfWordInKit = uppercaseLettersKit.indexOf(searchLetter);
-			// console.log(indexOfWordInKit);
-			newIndex = (indexOfWordInKit + offset) % kitsLength;
-			outputWord += uppercaseLettersKit[newIndex];
-			// console.log(outputWord);
-		} else if (lowercaseLettersKit.includes(searchLetter)) {
-			// console.log(searchLetter);
-			indexOfWordInKit = lowercaseLettersKit.indexOf(searchLetter);
-			newIndex = (indexOfWordInKit + offset) % kitsLength;
-			outputWord += lowercaseLettersKit[newIndex];
-			// console.log(outputWord);
-		}
+        if (indexOfLettersKit === 33 && offset === -1) {
+            indexOfLettersKit = 65;
+        } else if (indexOfLettersKit === 65 && offset === 1) {
+            indexOfLettersKit = 33;
+        } else if (indexOfLettersKit === 0 && offset === -1) {
+            indexOfLettersKit = 32;
+        } else if (indexOfLettersKit === 32 && offset === 1) {
+            indexOfLettersKit = 0;
+        } else {
+            indexOfLettersKit = indexOfLettersKit + offset;
+        }
+        outputWord += lettersKit[indexOfLettersKit];
 
-	} else if (offset === -1) {
-		if (uppercaseLettersKit.includes(searchLetter)) {
-			// console.log(searchLetter);
-			indexOfWordInKit = uppercaseLettersKit.indexOf(searchLetter);
-			// console.log(indexOfWordInKit);
-			newIndex = (indexOfWordInKit + offset);
-			if (newIndex < 0) newIndex += kitsLength;
-			outputWord += uppercaseLettersKit[newIndex];
-			// console.log(outputWord);
-		} else if (lowercaseLettersKit.includes(searchLetter)) {
-			// console.log(searchLetter);
-			indexOfWordInKit = lowercaseLettersKit.indexOf(searchLetter);
-			newIndex = (indexOfWordInKit + offset);
-			if (newIndex < 0) newIndex += kitsLength;
-			outputWord += lowercaseLettersKit[newIndex];
-			// console.log(outputWord);
-		}
-	} else {
-		console.log("Variable offset has non-valid value");
-	}
+    } else {
+        outputWord += acceptedWord.charAt(i);
+    }
 }
 
 console.log("\n" + outputWord);
