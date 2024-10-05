802377b2d2a60f8b85ccbf202b2d90429b2c8932
Kirill <stoporenko.k@gmail.com>
2024-10-05 10:46:26 +0700

    solved task 3

--- 5-functions/task_3.js
+++ 5-functions/task_3.js
@@ -6,3 +6,20 @@
 Например при подаче числа 19 (1+9=10>9, потому 1+0=1) выводится 1
 */
 
+function sumNumbers(number) {
+    const newArray = number.toString().split("");
+    let sumOfArray = 0;
+
+    for (let i = 0; i < newArray.length; i++) {
+        sumOfArray += +newArray[i];
+    }
+
+    if (sumOfArray > 9) {
+        return sumNumbers(sumOfArray);
+    } else {
+        return sumOfArray;
+    }
+
+}
+
+console.log(sumNumbers(19));
