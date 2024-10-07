da06d2f3fbb5a973e6b214da001a8985692cd90a
Kirill <stoporenko.k@gmail.com>
2024-10-07 21:53:29 +0700

    solved task 1

---
+++ 7-practice_1/task_1.js
@@ -0,0 +1,19 @@
+/*
+Напишите функцию, которая принимает на вход массив слов
+и возвращает отсортированный массив по следующему критерию: количество гласных букв.
+
+Массив должен быть отсортирован по возрастанию количества гласных букв в слове.
+*/
+
+const words = ["umbrella", "apple", "ocean", "independent", "education", "elephant", "island", "universe", "environment", "queue"];
+
+function countVowels(word) {
+	const vowels = "aueoyi";
+	return [...word].filter((char) => vowels.includes(char)).length;
+}
+
+function sortByVowels(array) {
+	return [...array].sort((word1, word2) => countVowels(word1) - countVowels(word2));
+}
+
+console.log(sortByVowels(words));
