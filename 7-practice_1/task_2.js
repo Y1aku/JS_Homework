2b3d520a346207e64ddf65d9eafd507301bef2d7
Kirill <stoporenko.k@gmail.com>
2024-10-12 02:55:44 +0700

    solved task 2

--- 7-practice_1/task_2.js
+++ 7-practice_1/task_2.js
@@ -7,3 +7,12 @@
 
 const prices = [64, 7556, 345, 7556, 345, 7556, 345, 7556, 433, 345, 756, 123, 942, 3112, 421, 9341, 1212, 8, 43, 41, 345, 341, 21, 321, 123];
 
+function getTotalPrice(arr) {
+	return arr.reduce((acc, element) => acc + element, 0);
+}
+
+function getAveragePrice(arr) {
+	return getTotalPrice(arr) / arr.length;
+}
+
+console.log(`Итого: $${getTotalPrice(prices)}, средняя цена товара: $${getAveragePrice(prices)}`);
