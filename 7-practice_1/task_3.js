ec8ba3d014d77e855e63aed0abf145a2643ff1ed
Kirill <stoporenko.k@gmail.com>
2024-10-18 02:04:42 +0700

    solved task 3 with reduce

--- 7-practice_1/task_3.js
+++ 7-practice_1/task_3.js
@@ -3,10 +3,20 @@
 и возвращает количество раз, когда значение встречается в массиве.
 */
 
+// const array = [1, 2, 2, 3, 2, 4, 5];
+
+// function countOccurrences(arr, num) {
+// 	return arr.filter((element) => element === num).length;
+// }
+
+// console.log(countOccurrences(array, 4)); // 3
+
+// Решение через reduce
+
 const array = [1, 2, 2, 3, 2, 4, 5];
 
 function countOccurrences(arr, num) {
-	return arr.filter((element) => element === num).length;
+	return arr.reduce((result, element) => (element === num ? ++result : result), 0);
 }
 
-console.log(countOccurrences(array, 4)); // 3
+console.log(countOccurrences(array, 2)); // 3
