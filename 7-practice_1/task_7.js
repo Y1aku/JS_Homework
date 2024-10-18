0203e906a68fdd56bb1de9074b9c14009c772bcf
Kirill <stoporenko.k@gmail.com>
2024-10-19 03:28:37 +0700

    solved task 7 vers.1

---
+++ 7-practice_1/task_7.js
@@ -0,0 +1,19 @@
+/*
+Напишите функцию, которая принимает массив чисел и возвращает новый массив, 
+где в каждом числе переставлены цифры так, 
+чтобы число получилось максимально возможным, например: 1234 => 4321
+*/
+
+// Решение 1
+
+const arr = [123, 542, 968, 352, 1, 308];
+
+function getGreatestTransformedNumbers(arr) {
+	return arr.map((element, index, array) => createMaxNumber(element));
+}
+
+function createMaxNumber(number) {
+	return +[...number.toString()].sort((a, b) => +b - +a).join("");
+}
+
+console.log(getGreatestTransformedNumbers(arr));
