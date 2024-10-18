f9d8e3fdb6b75cc4d19114861efe80df9660cd2c
Kirill <stoporenko.k@gmail.com>
2024-10-19 02:27:45 +0700

    solved task 5 vers.3

--- 7-practice_1/task_5.js
+++ 7-practice_1/task_5.js
@@ -17,17 +17,33 @@
 
 // Решение 2
 
+// const array = [false, 1, 0, 1, 2, 0, 1, 3, "a"];
+
+// function moveZeros(arr) {
+//     const arrayWithoutZeroes = arr.filter(element => element !== 0);
+//     const numberOfZeroes = arr.length - arrayWithoutZeroes.length;
+    
+//     for (let i = 0; i < numberOfZeroes; i++) {
+//         arrayWithoutZeroes.push(0);
+//     }
+    
+//     return arrayWithoutZeroes;
+// }
+
+// console.log(moveZeros(array));
+
+// Решение 3
+
 const array = [false, 1, 0, 1, 2, 0, 1, 3, "a"];
 
 function moveZeros(arr) {
-    const arrayWithoutZeroes = arr.filter(element => element !== 0);
-    const numberOfZeroes = arr.length - arrayWithoutZeroes.length;
-    
-    for (let i = 0; i < numberOfZeroes; i++) {
-        arrayWithoutZeroes.push(0);
+    for (let i = 0; i < arr.length; i++) {
+	    if (arr[i] === 0) {
+		    arr.splice(i, 1);
+	        arr.push(0);
+	    }
     }
-    
-    return arrayWithoutZeroes;
+    return arr;
 }
 
 console.log(moveZeros(array));
