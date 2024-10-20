fcb4d9003e7cc024634b9988d49db9601cd0d808
Kirill <stoporenko.k@gmail.com>
2024-10-20 23:46:11 +0700

    solution №1 of extra task using map()

--- 8-objects/task_3_from_lesson.js
+++ 8-objects/task_3_from_lesson.js
@@ -29,38 +29,59 @@
 ]
 */
 
+// Мое решение
+
+// const arrayOfNames = ["Anatoly", "Maria", "Pavel", "Svetlana"];
+// const arrayOfSalaries = [1000, 2000, 500, 0];
+// const arrayOfProfessions = ["AQA", "Developer", "DevOps", "QA"];
+
+// function getArrayOfObjects(arr1, arr2, arr3) {
+//     // Создание массива
+//     const newArray = [];
+
+//     // Условие, при котором проверяеся одинаковая длина всех массивов
+
+//     if (arr1.length === arr2.length && arr2.length === arr3.length) {
+        
+//         // Создание цикла
+//         for (let i = 0; i < arr1.length; i++) {
+
+//             // Создание объекта
+//             const newObject = new Object();
+
+//             // Создание ключей и Добавление значений к ключам
+//             newObject.name = arr1[i];
+//             newObject.salary = arr2[i];
+//             newObject.profession = arr3[i];
+
+//             // Добавление объекта к массиву
+//             newArray.push(newObject);
+//         }
+//     } else {
+//         console.log("Ошибка: разное кол-во элементов в массивах");
+//     }
+
+//     // Вывод
+// 	return newArray;
+// };
+
+// console.log(getArrayOfObjects(arrayOfNames, arrayOfSalaries, arrayOfProfessions));
+
+// Решение 1 с урока
+
 const arrayOfNames = ["Anatoly", "Maria", "Pavel", "Svetlana"];
 const arrayOfSalaries = [1000, 2000, 500, 0];
 const arrayOfProfessions = ["AQA", "Developer", "DevOps", "QA"];
 
-function getArrayOfObjects(arr1, arr2, arr3) {
-    // Создание массива
-    const newArray = [];
-
-    // Условие, при котором проверяеся одинаковая длина всех массивов
-
-    if (arr1.length === arr2.length && arr2.length === arr3.length) {
-        
-        // Создание цикла
-        for (let i = 0; i < arr1.length; i++) {
-
-            // Создание объекта
-            const newObject = new Object();
-
-            // Создание ключей и Добавление значений к ключам
-            newObject.name = arr1[i];
-            newObject.salary = arr2[i];
-            newObject.profession = arr3[i];
-
-            // Добавление объекта к массиву
-            newArray.push(newObject);
-        }
-    } else {
-        console.log("Ошибка: разное кол-во элементов в массивах");
-    }
-
-    // Вывод
-	return newArray;
-};
+function getArrayOfObjects(names, salaries, professions) {
+	return names.map((name, index) => ({
+		name,
+		salary: salaries[index],
+		profession: professions[index]
+	}));
+}
 
 console.log(getArrayOfObjects(arrayOfNames, arrayOfSalaries, arrayOfProfessions));
+
+// Решение 2 с урока
+
