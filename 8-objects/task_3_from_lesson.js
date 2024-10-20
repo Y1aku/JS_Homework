e1722a2d116f533f53532bd1b843813a71b93aa3
Kirill <stoporenko.k@gmail.com>
2024-10-20 20:22:16 +0700

    solved extra task from lesson

---
+++ 8-objects/task_3_from_lesson.js
@@ -0,0 +1,66 @@
+/*
+Есть массив имен `["Anatoly", "Maria", "Pavel", "Svetlana"]`,
+массив зарплат `[1000, 2000, 500, 0]`
+и массив профессий `["AQA", "Developer", "DevOps", "QA"]`.
+
+Нужно написать функцию, которая будет принимать на вход 3 массива и возвращать объект массивов следующего вида:
+
+[
+	{
+		name: "Anatoly",
+		salary: 1000,
+		profession: "AQA"
+	},
+	{
+		name: "Maria",
+		salary: 2000,
+		profession: "Developer"
+	},
+	{
+		name: "Pavel",
+		salary: 500,
+		profession: "DevOps"
+	},
+	{
+		name: "Svetlana",
+		salary: 0,
+		profession: "QA"
+	}
+]
+*/
+
+const arrayOfNames = ["Anatoly", "Maria", "Pavel", "Svetlana"];
+const arrayOfSalaries = [1000, 2000, 500, 0];
+const arrayOfProfessions = ["AQA", "Developer", "DevOps", "QA"];
+
+function getArrayOfObjects(arr1, arr2, arr3) {
+    // Создание массива
+    const newArray = [];
+
+    // Условие, при котором проверяеся одинаковая длина всех массивов
+
+    if (arr1.length === arr2.length && arr2.length === arr3.length) {
+        
+        // Создание цикла
+        for (let i = 0; i < arr1.length; i++) {
+
+            // Создание объекта
+            const newObject = new Object();
+
+            // Создание ключей и Добавление значений к ключам
+            newObject.name = arr1[i];
+            newObject.salary = arr2[i];
+            newObject.profession = arr3[i];
+
+            // Добавление объекта к массиву
+            newArray.push(newObject);
+        }
+    } else {
+        console.log("Ошибка: разное кол-во элементов в массивах");
+    }
+
+    // Вывод
+	return newArray;
+};
+
+console.log(getArrayOfObjects(arrayOfNames, arrayOfSalaries, arrayOfProfessions));
