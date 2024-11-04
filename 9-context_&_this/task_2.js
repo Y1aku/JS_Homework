f6326e1d4a73b819df1b8ab5f04719fa1dc60129
Kirill <stoporenko.k@gmail.com>
2024-11-05 04:03:14 +0700

    added condition of task 2

---
+++ 9-context_&_this/task_2.js
@@ -0,0 +1,25 @@
+/*
+У вас есть массив чисел.
+
+Напиши функцию `countOccurrences`, которая принимает массив чисел и возвращает объект с подсчётом каждого числа.
+
+Пример:
+
+const numbers = [1, 2, 2, 3, 4, 4, 4, 5];
+
+// Ожидается: { 1: 1, 2: 2, 3: 1, 4: 3, 5: 1 }
+
+function countOccurrences(arr) {
+  // ваш код
+}
+
+console.log(countOccurrences(numbers));
+*/
+
+const numbers = [1, 2, 2, 3, 4, 4, 4, 5];
+
+function countOccurrences(arr) {
+    // ваш код
+};
+  
+console.log(countOccurrences(numbers));
