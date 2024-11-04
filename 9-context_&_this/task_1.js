665ccddfea98bfa5cdf94c94c2056928ec80cfa3
Kirill <stoporenko.k@gmail.com>
2024-11-04 22:04:33 +0700

    solved task 1.1

--- 9-context_&_this/task_1.js
+++ 9-context_&_this/task_1.js
@@ -7,3 +7,15 @@ ${greetingsWord}, my name is ${name}, i'm ${age} and my salary is ${salary}
 
 Значения в строке должны ссылаться на контекст ЭТОГО ОБЪЕКТА, без подмен.
 */
+
+const qa = {
+    name: 'Alex',
+    age: 20,
+    salary:'1200 $',
+    getInfo: function(greetingsWord) {
+        return `${greetingsWord}, my name is ${this.name}, i'm ${this.age} and my salary is ${this.salary}`;
+    }
+};
+
+console.log(qa.getInfo('Hello!'));
+
