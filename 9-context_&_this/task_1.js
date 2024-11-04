2b7b76db37e51fbfdc49c1ef92422d99e21a6310
Kirill <stoporenko.k@gmail.com>
2024-11-04 23:09:53 +0700

    solved task 1.3

--- 9-context_&_this/task_1.js
+++ 9-context_&_this/task_1.js
@@ -37,6 +37,39 @@ const anotherQa = {
     salary: '3000 $'
 };
 
-console.log(qa.getInfo.bind(anotherQa)('Hello!'));
-console.log(qa.getInfo.call(anotherQa, 'Hello!'));
-console.log(qa.getInfo.apply(anotherQa, ['Hello!']));
+// console.log(qa.getInfo.bind(anotherQa)('Hello!'));
+// console.log(qa.getInfo.call(anotherQa, 'Hello!'));
+// console.log(qa.getInfo.apply(anotherQa, ['Hello!']));
+
+/*
+3. Closures
+
+Создайте функцию `createCounter()`
+
+Создайте в функции `createCounter` переменную `count`, которая будет равна `0`
+
+Верните из функции `createCounter` новую функцию
+
+В теле новой функции реализуйте увеличение `count` на + 1 при каждом вызове функции
+
+После увеличение каунтера выводите в консоль `Function was called ${count} times`
+
+Создайте переменную `functionCallCounter`, в которой будет лежать результат `createCounter()`
+
+Вызовите functionCallCounter() 5 раз, убедитесь что в консоли верно выводятся данные
+*/
+
+function createCounter() {
+    let count = 0;
+    return () => {
+        count++;
+        console.log(`Function was called ${count} times`);
+    }
+};
+
+const functionCallCounter = createCounter();
+functionCallCounter();
+functionCallCounter();
+functionCallCounter();
+functionCallCounter();
+functionCallCounter();
